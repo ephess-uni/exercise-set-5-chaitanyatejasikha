f8909ec56e72c7c0616292c3d25582da0c181171
chaitanyatejasikha <123258761+chaitanyatejasikha@users.noreply.github.com>
2023-02-28 10:37:35 +0530

    Update ex_5_3.py
    updated solution

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -7,9 +7,13 @@ This module contains an entry point that:
 """
 import numpy as np
 from argparse import ArgumentParser
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
 
 if __name__ == "__main__":
-    import argparse, os
+    import os
     parser = argparse.ArgumentParser(description='This program applies a standard scale transform to the data in infile and writes it to outfile.')
     parser.add_argument('infile',help='provide input file path',nargs='?')
     parser.add_argument('outfile',help='provide output file path',nargs='?')
@@ -17,6 +21,7 @@ if __name__ == "__main__":
     raw_data = np.loadtxt(args.infile)
     normed = (raw_data - raw_data.mean(axis=0)) / raw_data.std(axis=0)
     processed = normed
+    root_dir = get_repository_root()
     os.makedirs(root_dir / "outputs", exist_ok=True)
     np.savetxt(args.outfile, processed, fmt='%.2e')
 
