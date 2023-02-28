11671dbe8dbb095c264f431e7deed83a56ab6d63
chaitanyatejasikha <123258761+chaitanyatejasikha@users.noreply.github.com>
2023-02-28 09:59:30 +0530

    Update ex_5_1.py
    added import argparse statement

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -11,6 +11,7 @@ def main(infile):
 
 
 if __name__ == "__main__":
+    import argparse
     parser = argparse.ArgumentParser(description="This program prints the number of lines in infile.")
     parser.add_argument('infile',help='provide file path',nargs='?')
     args = parser.parse_args()
